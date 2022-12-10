6880a1ec6d7dc6fba6a833495ffdd44a542ed5a9
Pavel <rut100500@yandex.ru>
2022-12-10 10:26:56 +0300

    Упорядочить строки по убыванию двумерного рандомного массива

---
+++ task54/Program.cs
@@ -0,0 +1,83 @@
+﻿//  Задайте двумерный массив. Напишите метод, который упорядочит по убыванию 
+// элементы каждой строки двумерного массива. И продемонстрируйте его работу.
+/*Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2*/
+
+
+Console.WriteLine("Введите количество строк задаваемого массива - m");
+bool isParsedM = int.TryParse(Console.ReadLine(), out int m);
+
+Console.WriteLine("Введите количество столбцов задаваемого массива - n");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+
+if(!isParsedM || !isParsedN)
+{
+    Console.WriteLine("Ошибочный ввод! Не цифра! ПереВВедите!");
+    return;
+}
+
+int[,] array = CreateRandom2DArray(m, n);
+Console.WriteLine("Сгенерированный исходный массив:");
+Print2DArray(array);
+
+Console.WriteLine();
+int[,] arrangered = ArrangeInDecreaseColumn(array);
+Console.WriteLine("Упорядоченный по убываю массив:");
+Print2DArray(arrangered);
+Console.WriteLine();
+
+int[,] CreateRandom2DArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+
+    Random random = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(1, 9);
+        }
+    }
+    return array;
+}
+
+void Print2DArray (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ArrangeInDecreaseColumn (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1]) 
+                {
+                    int temp = array[i, k];
+                    array[i, k] = array[i, k + 1];
+                    array[i, k + 1] = temp;
+                }
+            }
+        }
+        
+    }
+    return array;
+}
